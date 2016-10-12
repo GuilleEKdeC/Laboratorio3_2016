a1e69729291208d7ba11e69fd22b0cfd776600d6
Guille <gecian@yahoo.com.ar>
2016-10-12 15:49:51 -0300

    xml Actividad Nueva Oferta Laboral (agregar scroll)

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio3v2/Categoria.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio3v2/Categoria.java
@@ -3,6 +3,36 @@ package dam.isi.frsf.utn.edu.ar.laboratorio3v2;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+
+
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Button;
+
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+
 
 public class Categoria implements Serializable {
 
@@ -11,8 +41,10 @@ public class Categoria implements Serializable {
     private String descripcion;     //Nombre de la Categoría
     private List<Trabajo> trabajos; //Lista con todos los TRABAJOS de ésta categoría
 
+
     //--------Declaración de Constante--------//
     public static final Categoria[] CATEGORIAS_MOCK= new Categoria[]{
+
             new Categoria(1,"Arquitecto"),
             new Categoria(2,"Desarrollador"),
             new Categoria(3,"Tester"),
--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio3v2/NuevaOfLaboral.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio3v2/NuevaOfLaboral.java
@@ -2,16 +2,52 @@ package dam.isi.frsf.utn.edu.ar.laboratorio3v2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class NuevaOfLaboral extends AppCompatActivity implements Serializable {
+
+    //Se crea un ArrayList de tipo Categorías
+    //private ArrayList<String> listaCategorías;
+
+    String categoria;
+    String[] listaCategorias;
 
     /*-----------------------------------------On Create------------------------------------------*/
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oflaboral);
+
+        //inicializo variables
+        categoria = null;
+        listaCategorias = null;
+
+
+        // Setea el Spinner con los horarios posibles, cargados en el recurso arrays.xml
+        listaCategorias = getResources().getStringArray(R.array.categorías);
+        ArrayAdapter adaptadorCategorias = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaCategorias);
+        Spinner spinnerCategorias;// = new Spinner();
+        spinnerCategorias = (Spinner)findViewById(R.id.spinner);
+        spinnerCategorias.setAdapter(adaptadorCategorias);
+
+        //Manejo de la Selección de un Item (comida)
+        spinnerCategorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Guardamos en una variable la Categoría seleccionada
+              //  categoria = (String) spinnerCategorias.getItemAtPosition(position);
+            }
+        });
+        //-------------------Fin del Spinner-----------------------//
+
 
 
 
